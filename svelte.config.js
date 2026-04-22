6480344b241d6c34ec1b2d783086e97d2f738d10
karmajinx-og <info@karmajinx.com>
2026-04-22 21:01:28 +1000

    Create svelte.config.js

---
+++ svelte.config.js
@@ -0,0 +1,7 @@
+import adapter from '@sveltejs/adapter-cloudflare'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+const config = {
+  preprocess: vitePreprocess(),
+  kit: { adapter: adapter(), alias: { '$lib': './src/lib' } }
+}
+export default config
